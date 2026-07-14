362c540ac4bfa92fc62b2bfc433003085dca946f
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-07-15 00:10:02 +0530

    student table created

--- 01-basics.sql
+++ 01-basics.sql
@@ -3,6 +3,21 @@ CREATE TABLE students (
     --serial auto incremented integer, primary key is unique
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50),
+
     email VARCHAR(322) UNIQUE NOT NULL,
     phone_number VARCHAR(10) UNIQUE,
+    country_code VARCHAR(4),
+
+    age INT CHECK (age > 12),
+
+    current_status VARCHAR(20) DEFAULT 'active' CHECK (current_status IN ('active', 'graduated', 'dropped_out')),
+
+    masterji_handle VARCHAR(50) UNIQUE,
+
+    has_joined_masterji BOOLEAN DEFAULT FALSE,
+
+    current_score INT DEFAULT 0 CHECK(current_score >= 0 AND current_score <= 100),
+
+    enrollment_date DATETIME DEFAULT CURRENT_DATETIME
+
 )
