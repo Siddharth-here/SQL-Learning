3ef08a1c9c95a4c42eefbeb563a824c4ac790784
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-07-14 23:55:17 +0530

    student table creation

--- 01-basics.sql
+++ 01-basics.sql
@@ -1,3 +1,8 @@
 CREATE TABLE students (
-    
+    student_id SERIAL PRIMARY KEY,
+    --serial auto incremented integer, primary key is unique
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50),
+    email VARCHAR(322) UNIQUE NOT NULL,
+    phone_number VARCHAR(10) UNIQUE,
 )
